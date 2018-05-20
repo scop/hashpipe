b974475290454d4892ad48f1095acd831cdcc33b
Ville Skyttä <ville.skytta@iki.fi>
2018-05-20 16:06:55 +0200

    Fix test regexes per Python 3.7 changes
    See item starting with "The result of splitting a string on a regular
    expression that could match an empty string has been changed" in
    https://docs.python.org/3.7/whatsnew/3.7.html and
    https://bugs.python.org/issue33585

--- test_hashpipe.py
+++ test_hashpipe.py
@@ -16,6 +16,7 @@ def test_ref_nongrouping():
         {
             "key": b"",
             "data": b"",
+            "regex": re.compile(b".*"),
             "hashes": {
                 "md5": b"74e6f7298a9c2d168935f58c001bad88",
                 "sha1": b"fbdb1d1b18aa6c08324b7d64b71fb76370690e1d",
@@ -26,6 +27,7 @@ def test_ref_nongrouping():
         {
             "key": b"key",
             "data": b"The quick brown fox jumps over the lazy dog",
+            "regex": re.compile(b".+"),
             "hashes": {
                 "md5": b"80070713463e7749b90c2dc24911e275",
                 "sha1": b"de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9",
@@ -38,6 +40,7 @@ def test_ref_nongrouping():
         {
             "key": bytes.fromhex("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b"),
             "data": b"Hi There",
+            "regex": re.compile(b".+"),
             "hashes": {
                 "md5": b"9294727a3638bb1c13f48ef8158bfc9d",
             },
@@ -45,6 +48,7 @@ def test_ref_nongrouping():
         {
             "key": bytes.fromhex("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b"),
             "data": b"Hi There",
+            "regex": re.compile(b".+"),
             "hashes": {
                 "sha1": b"b617318655057264e28bc0b6fb378c8ef146be00",
             },
@@ -52,6 +56,7 @@ def test_ref_nongrouping():
         {
             "key": b"Jefe",
             "data": b"what do ya want for nothing?",
+            "regex": re.compile(b".+"),
             "hashes": {
                 "md5": b"750c783e6ab0b503eaa86e310a5db738",
                 "sha1": b"effcdf6ae5eb2fa2d27416d5f184df9c259a7c79",
@@ -60,6 +65,7 @@ def test_ref_nongrouping():
         {
             "key": bytes.fromhex("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
             "data": bytes.fromhex("dd" * 50),
+            "regex": re.compile(b".+"),
             "hashes": {
                 "md5": b"56be34521d144c88dbb8c733f0e8b3f6",
             },
@@ -67,6 +73,7 @@ def test_ref_nongrouping():
         {
             "key": bytes.fromhex("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
             "data": bytes.fromhex("dd" * 50),
+            "regex": re.compile(b".+"),
             "hashes": {
                 "sha1": b"125d7342b9ac11cd91a39af48aa17b4f63f175d3",
             },
@@ -75,6 +82,7 @@ def test_ref_nongrouping():
             "key": bytes.fromhex(
                 "0102030405060708090a0b0c0d0e0f10111213141516171819"),
             "data": bytes.fromhex("cd" * 50),
+            "regex": re.compile(b".+"),
             "hashes": {
                 "md5": b"697eaf0aca3a3aea3a75164746ffaa79",
                 "sha1": b"4c9007f4026250c6bc8414f9bf50c86c2d7235da",
@@ -83,6 +91,7 @@ def test_ref_nongrouping():
         {
             "key": bytes.fromhex("0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c"),
             "data": b"Test With Truncation",
+            "regex": re.compile(b".+"),
             "hashes": {
                 "md5": b"56461ef2342edc00f9bab995690efd4c",
             },
@@ -90,6 +99,7 @@ def test_ref_nongrouping():
         {
             "key": bytes.fromhex("0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c"),
             "data": b"Test With Truncation",
+            "regex": re.compile(b".+"),
             "hashes": {
                 "sha1": b"4c1a03424b55e07fe7f27be1d58bb9324a9a5a04",
             },
@@ -97,6 +107,7 @@ def test_ref_nongrouping():
         {
             "key": bytes.fromhex("aa" * 80),
             "data": b"Test Using Larger Than Block-Size Key - Hash Key First",
+            "regex": re.compile(b".+"),
             "hashes": {
                 "md5": b"6b1ab7fe4bd7bf8f0b62e6ce61b9d0cd",
                 "sha1": b"aa4ae5e15272d00e95705637ce8a3b55ed402112",
@@ -106,18 +117,18 @@ def test_ref_nongrouping():
             "key": bytes.fromhex("aa" * 80),
             "data": b"Test Using Larger Than Block-Size Key "
                     b"and Larger Than One Block-Size Data",
+            "regex": re.compile(b".+"),
             "hashes": {
                 "md5": b"6f630fad67cda0ee1fb1f562db3aa53e",
                 "sha1": b"e8e99d0f45237d786d6bbaa7965c7808bbff1a91",
             },
         },
     ]
-    regex = re.compile(b".*")
 
     for case in cases:
         for algorithm, hash_ in case["hashes"].items():
             assert hash_matches(
-                algorithm=algorithm, regex=regex,
+                algorithm=algorithm, regex=case["regex"],
                 data=case["data"], key=case["key"],
             ) == _format_hash(hash_)
 
