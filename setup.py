ecda47552f3e9318c88433325d699a43f2a0b79d
Ville Skyttä <ville.skytta@iki.fi>
2019-02-05 19:20:29 +0200

    Add build/dist support files

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Hashpipe setup."""
+
+import setuptools  # type: ignore
+
+
+def get_version() -> str:
+    """Extract version number."""
+    fname = "hashpipe/__init__.py"
+    with open(fname) as file_:
+        for line in file_:
+            if line.startswith("__version__"):
+                return line.split("=")[-1].strip("\"'\r\n ")
+    raise Exception("No __version__ in %s!" % fname)
+
+
+if __name__ == "__main__":
+    setuptools.setup(version=get_version())
