55e6bd7f68487bde6478934f91133e479eded543
Ville Skyttä <ville.skytta@iki.fi>
2019-08-25 08:19:26 +0300

    .dir-locals.el, .editorconfig: add some editor configs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((python-mode
-  (eval . (add-hook 'before-save-hook 'blacken-buffer nil t))))
+  (eval add-hook 'before-save-hook 'blacken-buffer nil t)))
