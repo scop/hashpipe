945424b43298861a4d73c1da097f0f4e70b21a5d
Ville Skyttä <ville.skytta@iki.fi>
2021-08-22 16:44:03 +0300

    chore(setup.py): `open` with explicit encoding
    Hushes pylint's `unspecified-encoding`.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools  # type: ignore[import]
 def get_version() -> str:
     """Extract version number."""
     fname = "hashpipe/__init__.py"
-    with open(fname) as file_:
+    with open(fname, encoding="utf-8") as file_:
         for line in file_:
             if line.startswith("__version__"):
                 return line.split("=")[-1].strip("\"'\r\n ")
