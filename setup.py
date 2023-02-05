0a236a95e11b5b794c0ddd9d403f58042ebaaa94
Ville Skyttä <ville.skytta@iki.fi>
2023-02-06 00:23:14 +0200

    build(setup): raise `NameError` on `hashpipe.__version__` not found
    Thanks to pylint's `broad-exception-raised`.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_version() -> str:
         for line in file_:
             if line.startswith("__version__"):
                 return line.split("=")[-1].strip("\"'\r\n ")
-    raise Exception("No __version__ in %s!" % fname)
+    raise NameError("No __version__ in %s!" % fname)
 
 
 if __name__ == "__main__":
